e8ebddb0bbe2360e0d67320317a0dd2f36a696ca
konatham naga mukesh <konathamnagamukesh2005@gmail.com>
2024-03-01 17:58:24 +0530

    change 7

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,20 @@
 const express=require('express')
 const router=express.Router()
-
-
+const app=express()
+const bodyParser=require("body-parser");
+const encoder=bodyParser.urlencoded();
+app.use(express.urlencoded({extended:false}))
 
 router.get('/',(req,res)=>{
     res.render('users')
+})
+
+router.post('/',encoder,(req,res)=>{
+    const username=req.body.name 
+    const password=req.body.password
+    console.log("username:",username);
+    console.log(password);
+    res.send("login successful");
 })
 
 router.get('/new',(req,res)=>{
